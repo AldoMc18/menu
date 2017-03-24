988bc5c315717dd855c82c73a9e97b6422f8a4bc
cambrano <cambrano@maquina>
2017-03-24 12:10:34 -0600

    se modifico

--- src/miprimermenu/usarOperaciones.java
+++ src/miprimermenu/usarOperaciones.java
@@ -14,18 +14,45 @@ import java.util.Scanner;
 public class usarOperaciones {
     public static void main(String args[]){
         Scanner in=new Scanner(System.in);
-        
+        int sel=0,num1,num2;
         operaciones objeto=new operaciones();
         
-        
+        do{
+         System.out.println("Que desea hacer usted?");
         System.out.println("1.-Suma");
         System.out.println("2.-Resta");
         System.out.println("3.- Salir");
+        sel=in.nextInt();
         
-        int suma=objeto.suma(3, 4);
-        int resta=objeto.resta(3, 4);
         
-        System.out.println("La suma es:"+suma);
-        System.out.println("La resta es:"+resta);
+        
+        switch(sel){
+            
+            case 1: 
+                System.out.println("Ingresa el primer valor");
+                num1=in.nextInt();
+                System.out.println("Ingresa el segundo valor");
+                num2=in.nextInt();
+                int suma=objeto.suma(num1, num2);
+            System.out.println("La suma es:"+suma);
+            break;
+            
+            case 2:
+                System.out.println("Ingresa el primer valor");
+                num1=in.nextInt();
+                System.out.println("Ingresa el segundo valor");
+                num2=in.nextInt();
+                int resta=objeto.resta(num1, num2);
+            System.out.println("La resta es:"+resta);
+            break;
+            
+            case 3: System.exit(0);
+            break;
+        }
+        
+        }while(sel!=3);
+        
+        
+        
     }
 }
